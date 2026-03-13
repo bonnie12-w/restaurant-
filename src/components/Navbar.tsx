5b28adc8a10b0bf72a4c1eb213fe091bb78aa369
bonnie12-w <bonfacekuggie@gmail.com>
2026-03-13 15:26:58 +0300

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,9 +39,9 @@ const Navbar = () => {
           <span className="text-2xl">🔥</span>
           <div>
             <h1 className="font-display text-lg sm:text-xl font-bold gold-gradient-text leading-tight">
-              Savannah Flame
+              Cafe Deli Restaurant
             </h1>
-            <p className="text-[10px] tracking-[0.3em] uppercase text-muted-foreground">Grill</p>
+            <p className="text-[10px] tracking-[0.3em] uppercase text-muted-foreground">Cd</p>
           </div>
         </Link>
 
